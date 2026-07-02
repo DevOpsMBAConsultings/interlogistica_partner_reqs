6db2b1fc108ced63d6bcce39c86c9a5a9fc4d868
DevOpsMBAConsultings <devops@mbaconsultings.com>
2026-07-01 20:17:26 -0500

    Initial commit: Enforce required fields for partner

---
+++ __manifest__.py
@@ -0,0 +1,21 @@
+{
+    'name': 'Interlogistica Partner Requirements',
+    'version': '18.0.1.0.0',
+    'category': 'Sales/CRM',
+    'summary': 'Hace obligatorios los campos de teléfono, dirección, nombre y apellido en contactos.',
+    'description': """
+        Este módulo asegura que al crear o editar un contacto (res.partner) que sea individuo,
+        se completen obligatoriamente los campos:
+        - Nombre y Apellido (usando partner_firstname)
+        - Teléfono
+        - Dirección (Calle)
+    """,
+    'author': 'DevOpsMBAConsultings',
+    'depends': ['base', 'partner_firstname'],
+    'data': [
+        'views/res_partner_views.xml',
+    ],
+    'installable': True,
+    'application': False,
+    'license': 'LGPL-3',
+}
